e3d8d24ee8c25a0cbdcba71ec10e6276b0f7ec07
TxYms <nomine.tim@hotmail.com>
2024-12-23 15:24:33 +0100

    writer implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytic/ISymptomWriter.java
@@ -0,0 +1,16 @@
+package com.hemebiotech.analytic;
+
+
+
+import java.util.List;
+
+
+public interface ISymptomWriter {
+	
+	 public void writeSymptoms(List<String> symptoms);
+
+	List<String> writeSymptoms();
+
+	
+
+}
